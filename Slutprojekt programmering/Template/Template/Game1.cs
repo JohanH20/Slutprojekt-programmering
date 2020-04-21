229538e8cc270eccf8b7253c9089ad29ead09558
JohanH20 <54435441+JohanH20@users.noreply.github.com>
2020-04-21 13:53:05 +0200

    försökte fixa rörelsen men har inte helt funkat än

--- Slutprojekt programmering/Template/Template/Game1.cs
+++ Slutprojekt programmering/Template/Template/Game1.cs
@@ -33,7 +33,7 @@ namespace snake
             // TODO: Add your initialization logic here
 
             base.Initialize();
-            ormen = new Orm(20, 3, graphics.GraphicsDevice.Viewport.Bounds.Width / 2 - 20, graphics.GraphicsDevice.Viewport.Bounds.Height / 2 - 20);
+            ormen = new Orm(graphics.GraphicsDevice.Viewport.Bounds.Width / 2 - 20, graphics.GraphicsDevice.Viewport.Bounds.Height / 2 - 20);
 
             Random random = new Random();
             maten.Maten = new Vector2(random.Next(graphics.GraphicsDevice.Viewport.Bounds.Width), random.Next(graphics.GraphicsDevice.Viewport.Bounds.Height));
